11cc99a54919b7d11d13313da80cb54b25ffb621
waltercarmona9 <walter.carmona9@gmail.com>
2022-04-19 10:33:25 -0300

    pwav01.01

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React, { useState } from 'react';
+import Notes from './components/Notes';
+import './style.css';
 
-function App() {
+export default function App() {
+  let [list, setList] = useState([]);
+  let [newNote, setNewNote] = useState('');
+
+  function addNote() {
+    let addedNote = list.concat(newNote);
+    setList(addedNote);
+    setNewNote('');
+  }
+
+  function handleChange(e) {
+    setNewNote(e.target.value);
+  }
+
   return (
     <div>
        <h1>My Notes</h1>
-       <imput type="text" />
-       <button>Save</button>
+       <imput type="text" value={newNote} onChange={handleChange} />
+
+       <button> onClick={addNote}>Save</button>
+       <Notes data={list}/>
     </div>
   );
 }
